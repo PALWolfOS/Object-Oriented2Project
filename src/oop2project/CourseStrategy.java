7605ae82cd1c053bc2dd80fda8df6a9c2d3bab10
PALWolfOS <61064672+PALWolfOS@users.noreply.github.com>
2020-12-04 11:51:15 -0400

    Add files via upload

---
+++ src/oop2project/CourseStrategy.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop2project;
+
+/**
+ *
+ * @author joshu_000
+ */
+public interface CourseStrategy {
+   public void PrintCoursesCore();
+   public void PrintCoursesElective();
+}
---
+++ src/oop2project/OOP2Project.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop2project;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author joshu_000
+ */
+public class OOP2Project {
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        StudentPortal gui = new StudentPortal();
+        gui.setVisible(true); 
+        //gui.CorePanel.setVisible(true);
+            
+    }
+}
